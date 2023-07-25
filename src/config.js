f970abfa9abbd5a4fd7fbdb18221ab2672b077bb
stefy <rgarciac4@unemi.edu.ec>
2023-07-25 14:01:19 -0500

    Subida de archivos

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const PORT = 5000;
+export const MONGODB_URI =  "mongodb+srv://rgarciac4:12345@cluster0.hge64ii.mongodb.net/?retryWrites=true&w=majority";
+export const TOKEN_SECRET = "secret_clave";
+
+
